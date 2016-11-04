ae6a664af8ec72ad8c7c7a52a587072c8e8cfa88
Tomasz <thomas.kolonko@ilub.unibe.ch>
2016-11-04 08:53:13 +0100

    ilias52 ready

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'sys_not';
 $version = '1.4.1';
 $ilias_min_version = '5.0.0';
-$ilias_max_version = '5.1.999';
+$ilias_max_version = '5.2.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'fs@studer-raimann.ch';
 ?>
