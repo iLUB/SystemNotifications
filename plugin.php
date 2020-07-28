0d441a71e35e4377fb4dcc12df0331cd2379bb52
Timon Amstutz <timon.amstutz@bluewin.ch>
2020-07-28 17:15:06 +0200

    Update Plugin Version

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'sys_not';
 $version = '1.5.0';
 $ilias_min_version = '5.1.0';
-$ilias_max_version = '6.0.999';
+$ilias_max_version = '6.999.999';
 $responsible = 'studer + raimann ag';
 $responsible_mail = 'support@studer-raimann.ch';
